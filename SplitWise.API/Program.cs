ab4c8f971fa97ce1e615818d0ba1324b90e55a61
kanchantiwari <kanchantiwari.959@gmail.com>
2026-07-30 16:25:36 +0530

    chore: initialize solution with clean architecture

---
+++ SplitWise.API/Program.cs
@@ -0,0 +1,30 @@
+namespace SplitWise.API;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
+}
